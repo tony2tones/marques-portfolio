355e9f6e4487fddd8d6930ec6a3ce184e3c3ec60
Tony2tones777 <anthonymarques@outlook.com>
2020-07-13 18:00:24 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { WorkComponent } from './components/work/work.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
